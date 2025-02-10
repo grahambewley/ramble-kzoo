117bbd98234798784016f47daf5ce5548b6bb6d7
Graham Bewley <grahambewley@gmail.com>
2025-02-10 18:11:54 -0500

    Add info section

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ module.exports = {
     nextRideDate: '2023-11-01',
     nextRideTime: '10:00 AM',
     nextRideLocation: 'Central Park, NYC',
+    nextRideStravaRouteUrl: 'https://www.strava.com/routes/3295424537735572606',
+    nextRideInfo: 'Riding down the Kal-Haven. Coffee in the woods.',
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import { FaInstagram, FaCalendarAlt, FaClock, FaMapMarkerAlt } from "react-icons/fa";
+import { FaInstagram, FaCalendarAlt, FaClock, FaMapMarkerAlt, FaInfoCircle } from "react-icons/fa";
 import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
-  const { nextRideDate, nextRideTime, nextRideLocation } = publicRuntimeConfig;
+  const { nextRideDate, nextRideTime, nextRideLocation, nextRideStravaRouteUrl, nextRideInfo } = publicRuntimeConfig;
 
   return (
     <div className={styles.page}>
@@ -14,19 +14,34 @@ export default function Home() {
         <div className={styles.responsiveImage}>
           <Image src={"/bikes-and-coffee.png"} alt="Bikes and Coffee" layout="responsive" width={360} height={360} />
         </div>
-        <div className={styles.buttonContainer}>
-          <a href="https://www.strava.com/routes/3295424537735572606" className={styles.linkButton}>Strava Route</a>
-        </div>
         <div className={styles.nextRide}>
           <h2>Next Ride</h2>
-          <p><FaCalendarAlt /> {nextRideDate}</p>
-          <p><FaClock /> {nextRideTime}</p>
-          <p><FaMapMarkerAlt /> {nextRideLocation}</p>
+          <p className={styles.helperText}>(typically every other Sunday)</p>
+          <div className={styles.infoWrapper}>
+            <FaCalendarAlt className={styles.icon} />
+            <p>{nextRideDate}</p>
+          </div>
+          <div className={styles.infoWrapper}>
+          <FaClock className={styles.icon} />
+            <p>{nextRideTime}</p>
+          </div>
+          <div className={styles.infoWrapper}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <p>{nextRideLocation}</p>
+          </div>
+          <div className={styles.infoWrapper}>
+            <FaInfoCircle className={styles.icon} />
+            <p>{nextRideInfo}</p>
+          </div>
+          <div className={styles.buttonContainer}>
+            <a href={nextRideStravaRouteUrl} target="_blank" className={styles.linkButton}>Strava Route</a>
+            <a href="https://strava.app.link/EOnnVuoUSQb" target="_blank" className={`${styles.linkButton} ${styles.joinClub}`}>Join the Club!</a>
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
         <a href="https://instagram.com/graham.bewley">
-          <FaInstagram size={24} />
+          <FaInstagram size={30} />
         </a>
       </footer>
     </div>

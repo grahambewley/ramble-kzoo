4d373fedbc6f5216176fb3fd325ed41c4b61da0b
Graham Bewley <grahambewley@gmail.com>
2025-02-10 16:24:42 -0500

    initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicRuntimeConfig: {
+    nextRideDate: '2023-11-01',
+    nextRideTime: '10:00 AM',
+    nextRideLocation: 'Central Park, NYC',
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,32 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
+  const { nextRideDate, nextRideTime, nextRideLocation } = publicRuntimeConfig;
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.responsiveImage}>
+          <Image src={"/bikes-and-coffee.png"} alt="Bikes and Coffee" layout="responsive" width={360} height={360} />
+        </div>
+        <div className={styles.buttonContainer}>
+          <a href="https://www.strava.com/routes/3295424537735572606" className={styles.linkButton}>Strava Route</a>
+        </div>
+        <div className={styles.nextRide}>
+          <h2>Next Ride</h2>
+          <p>Date: {nextRideDate}</p>
+          <p>Time: {nextRideTime}</p>
+          <p>Meeting Location: {nextRideLocation}</p>
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a href="https://instagram.com/graham.bewley">
+          <FaInstagram size={24} />
         </a>
       </footer>
     </div>

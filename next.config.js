753501f9fbe1fa9d8919c1d2877b2b500fac9477
Graham Bewley <grahambewley@gmail.com>
2025-02-12 17:33:38 -0500

    Update config values!

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   publicRuntimeConfig: {
-    nextRideDate: 'March 9, 2025',
-    nextRideTime: '11AM',
+    nextRideDate: 'March 16, 2025',
+    nextRideTime: '11 AM',
     nextRideLocation: 'Bronson Park',
-    nextRideStravaRouteUrl: 'https://strava.app.link/vtzLeBl5SQb',
-    nextRideInfo: 'Meet at Bronson Park, ride down the Kal-Haven, make coffee in the woods',
+    nextRideStravaRouteUrl: 'https://www.strava.com/routes/3324865601747192018',
+    nextRideInfo: 'Meet at Bronson Park, ride down the Kal-Haven, make coffee in the woods.',
   },
 };

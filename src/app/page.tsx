e25c2ab3996bf6debc9c28bbcd078d658813d101
Graham Bewley <grahambewley@gmail.com>
2025-02-12 16:01:27 -0500

    Update IG icon size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a href="https://instagram.com/graham.bewley">
-          <FaInstagram size={30} />
+          <FaInstagram size={24} />
         </a>
       </footer>
     </div>
